9b89f8041409cb8a74321cc2e882c7dcbd90c8fa
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-03-03 00:21:49 +0900

    Change some comment

--- stm32f103/stm32f103.ino
+++ stm32f103/stm32f103.ino
@@ -329,8 +329,8 @@ const long interval = 1000;           // transmission interval (milliseconds)
 void setup() {
   Serial.begin(115200);
   bool ret = CANInit(CAN_1000KBPS, 0);  // CAN_RX mapped to PA11, CAN_TX mapped to PA12
-  //CANInit(CAN_1000KBPS, 2);  // CAN_RX mapped to PB8, CAN_TX mapped to PB9
-  //CANInit(CAN_1000KBPS, 3);  // CAN_RX mapped to PD0, CAN_TX mapped to PD1
+  //bool ret = CANInit(CAN_1000KBPS, 2);  // CAN_RX mapped to PB8, CAN_TX mapped to PB9
+  //bool ret = CANInit(CAN_1000KBPS, 3);  // CAN_RX mapped to PD0, CAN_TX mapped to PD1
   if (!ret) while(true);
 }
 
