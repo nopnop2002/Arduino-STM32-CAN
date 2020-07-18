de9489463a2ba0cd4e45c94cc77851902171f504
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-07-18 20:22:15 +0900

    Change ID

--- stm32f446/stm32f446.ino
+++ stm32f446/stm32f446.ino
@@ -520,7 +520,7 @@ void loop() {
       CAN_TX_msg.type = DATA_FRAME;
       if (CAN_TX_msg.len == 0) CAN_TX_msg.type = REMOTE_FRAME;
       CAN_TX_msg.format = EXTENDED_FORMAT;
-      CAN_TX_msg.id = 0x446;
+      CAN_TX_msg.id = 0x32F446;
     } else {
       CAN_TX_msg.type = DATA_FRAME;
       if (CAN_TX_msg.len == 0) CAN_TX_msg.type = REMOTE_FRAME;
