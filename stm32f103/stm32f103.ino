b47a0183274333a4dd187fd0e15c5da850c71710
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-08-21 14:19:35 +0900

    Added support for Generic F103 board

--- stm32f103/stm32f103.ino
+++ stm32f103/stm32f103.ino
@@ -1,7 +1,12 @@
+#include <assert.h>
+
 #define DEBUG 0
 
 /* Symbolic names for bit rate of CAN message                                */
 typedef enum {CAN_50KBPS, CAN_100KBPS, CAN_125KBPS, CAN_250KBPS, CAN_500KBPS, CAN_1000KBPS} BITRATE;
+
+/* Real speed for bit rate of CAN message                                    */
+uint32_t SPEED[6] = {50*1000, 100*1000, 125*1000, 250*1000, 500*1000, 1000*1000};
 
 /* Symbolic names for formats of CAN message                                 */
 typedef enum {STANDARD_FORMAT = 0, EXTENDED_FORMAT} CAN_FORMAT;
@@ -20,15 +25,162 @@ typedef struct
   uint8_t  type;      /* 0 - DATA FRAME, 1 - REMOTE FRAME                */
 } CAN_msg_t;
 
-typedef const struct
+typedef struct
 {
-  uint8_t TS2;
-  uint8_t TS1;
-  uint8_t BRP;
+    uint16_t baud_rate_prescaler;                /// [1 to 1024]
+    uint8_t time_segment_1;                      /// [1 to 16]
+    uint8_t time_segment_2;                      /// [1 to 8]
+    uint8_t resynchronization_jump_width;        /// [1 to 4] (recommended value is 1)
 } CAN_bit_timing_config_t;
 
-CAN_bit_timing_config_t can_configs[6] = {{2, 13, 45}, {2, 15, 20}, {2, 13, 18}, {2, 13, 9}, {2, 15, 4}, {2, 15, 2}};
+#define CAN_STM32_ERROR_UNSUPPORTED_BIT_RATE     1000
+#define CAN_STM32_ERROR_MSR_INAK_NOT_SET         1001
+#define CAN_STM32_ERROR_MSR_INAK_NOT_CLEARED     1002
+#define CAN_STM32_ERROR_UNSUPPORTED_FRAME_FORMAT 1003
 
+/*
+ * Calculation of bit timing dependent on peripheral clock rate
+ */
+int16_t ComputeCANTimings(const uint32_t peripheral_clock_rate,
+                          const uint32_t target_bitrate,
+                          CAN_bit_timing_config_t* const out_timings)
+{
+    if (target_bitrate < 1000)
+    {
+        return -CAN_STM32_ERROR_UNSUPPORTED_BIT_RATE;
+    }
+
+    assert(out_timings != NULL);  // NOLINT
+    memset(out_timings, 0, sizeof(*out_timings));
+
+    /*
+     * Hardware configuration
+     */
+    static const uint8_t MaxBS1 = 16;
+    static const uint8_t MaxBS2 = 8;
+
+    /*
+     * Ref. "Automatic Baudrate Detection in CANopen Networks", U. Koppe, MicroControl GmbH & Co. KG
+     *      CAN in Automation, 2003
+     *
+     * According to the source, optimal quanta per bit are:
+     *   Bitrate        Optimal Maximum
+     *   1000 kbps      8       10
+     *   500  kbps      16      17
+     *   250  kbps      16      17
+     *   125  kbps      16      17
+     */
+    const uint8_t max_quanta_per_bit = (uint8_t)((target_bitrate >= 1000000) ? 10 : 17);    // NOLINT
+    assert(max_quanta_per_bit <= (MaxBS1 + MaxBS2));
+
+    static const uint16_t MaxSamplePointLocationPermill = 900;
+
+    /*
+     * Computing (prescaler * BS):
+     *   BITRATE = 1 / (PRESCALER * (1 / PCLK) * (1 + BS1 + BS2))       -- See the Reference Manual
+     *   BITRATE = PCLK / (PRESCALER * (1 + BS1 + BS2))                 -- Simplified
+     * let:
+     *   BS = 1 + BS1 + BS2                                             -- Number of time quanta per bit
+     *   PRESCALER_BS = PRESCALER * BS
+     * ==>
+     *   PRESCALER_BS = PCLK / BITRATE
+     */
+    const uint32_t prescaler_bs = peripheral_clock_rate / target_bitrate;
+
+    /*
+     * Searching for such prescaler value so that the number of quanta per bit is highest.
+     */
+    uint8_t bs1_bs2_sum = (uint8_t)(max_quanta_per_bit - 1);    // NOLINT
+
+    while ((prescaler_bs % (1U + bs1_bs2_sum)) != 0)
+    {
+        if (bs1_bs2_sum <= 2)
+        {
+            return -CAN_STM32_ERROR_UNSUPPORTED_BIT_RATE;          // No solution
+        }
+        bs1_bs2_sum--;
+    }
+
+    const uint32_t prescaler = prescaler_bs / (1U + bs1_bs2_sum);
+    if ((prescaler < 1U) || (prescaler > 1024U))
+    {
+        return -CAN_STM32_ERROR_UNSUPPORTED_BIT_RATE;              // No solution
+    }
+
+    /*
+     * Now we have a constraint: (BS1 + BS2) == bs1_bs2_sum.
+     * We need to find such values so that the sample point is as close as possible to the optimal value,
+     * which is 87.5%, which is 7/8.
+     *
+     *   Solve[(1 + bs1)/(1 + bs1 + bs2) == 7/8, bs2]  (* Where 7/8 is 0.875, the recommended sample point location *)
+     *   {{bs2 -> (1 + bs1)/7}}
+     *
+     * Hence:
+     *   bs2 = (1 + bs1) / 7
+     *   bs1 = (7 * bs1_bs2_sum - 1) / 8
+     *
+     * Sample point location can be computed as follows:
+     *   Sample point location = (1 + bs1) / (1 + bs1 + bs2)
+     *
+     * Since the optimal solution is so close to the maximum, we prepare two solutions, and then pick the best one:
+     *   - With rounding to nearest
+     *   - With rounding to zero
+     */
+    uint8_t bs1 = (uint8_t)(((7 * bs1_bs2_sum - 1) + 4) / 8);       // Trying rounding to nearest first  // NOLINT
+    uint8_t bs2 = (uint8_t)(bs1_bs2_sum - bs1);  // NOLINT
+    assert(bs1_bs2_sum > bs1);
+
+    {
+        const uint16_t sample_point_permill = (uint16_t)(1000U * (1U + bs1) / (1U + bs1 + bs2));  // NOLINT
+
+        if (sample_point_permill > MaxSamplePointLocationPermill)   // Strictly more!
+        {
+            bs1 = (uint8_t)((7 * bs1_bs2_sum - 1) / 8);             // Nope, too far; now rounding to zero
+            bs2 = (uint8_t)(bs1_bs2_sum - bs1);
+        }
+    }
+
+    const bool valid = (bs1 >= 1) && (bs1 <= MaxBS1) && (bs2 >= 1) && (bs2 <= MaxBS2);
+
+    /*
+     * Final validation
+     * Helpful Python:
+     * def sample_point_from_btr(x):
+     *     assert 0b0011110010000000111111000000000 & x == 0
+     *     ts2,ts1,brp = (x>>20)&7, (x>>16)&15, x&511
+     *     return (1+ts1+1)/(1+ts1+1+ts2+1)
+     */
+    if ((target_bitrate != (peripheral_clock_rate / (prescaler * (1U + bs1 + bs2)))) ||
+        !valid)
+    {
+        // This actually means that the algorithm has a logic error, hence assert(0).
+        assert(0);  // NOLINT
+        return -CAN_STM32_ERROR_UNSUPPORTED_BIT_RATE;
+    }
+
+    out_timings->baud_rate_prescaler = (uint16_t) prescaler;
+    out_timings->resynchronization_jump_width = 1;      // One is recommended by UAVCAN, CANOpen, and DeviceNet
+    out_timings->time_segment_1 = bs1;
+    out_timings->time_segment_2 = bs2;
+
+    if (DEBUG) {
+      Serial.print("target_bitrate=");
+      Serial.println(target_bitrate);
+      Serial.print("peripheral_clock_rate=");
+      Serial.println(peripheral_clock_rate);
+  
+      Serial.print("timings.baud_rate_prescaler=");
+      Serial.println(out_timings->baud_rate_prescaler);
+      Serial.print("timings.time_segment_1=");
+      Serial.println(out_timings->time_segment_1);
+      Serial.print("timings.time_segment_2=");
+      Serial.println(out_timings->time_segment_2);
+      Serial.print("timings.resynchronization_jump_width=");
+      Serial.println(out_timings->resynchronization_jump_width);
+    }
+    return 0;
+}
+ 
 /**
  * Print registers.
 */ 
@@ -174,16 +326,28 @@ bool CANInit(BITRATE bitrate, int remap)
   //CAN1->MCR = 0x51UL;                 // Hardware initialization(No automatic retransmission)
   CAN1->MCR = 0x41UL;                   // Hardware initialization(With automatic retransmission)
    
-  // Set bit rates 
-  CAN1->BTR &= ~(((0x03) << 24) | ((0x07) << 20) | ((0x0F) << 16) | (0x1FF)); 
-  CAN1->BTR |=  (((can_configs[bitrate].TS2-1) & 0x07) << 20) | (((can_configs[bitrate].TS1-1) & 0x0F) << 16) | ((can_configs[bitrate].BRP-1) & 0x1FF);
+  // Set bit timing register 
+  CAN_bit_timing_config_t timings;
+  Serial.print("bitrate=");
+  Serial.println(bitrate);
+  uint32_t target_bitrate = SPEED[bitrate];
+  Serial.print("target_bitrate=");
+  Serial.println(target_bitrate);
+  int result = ComputeCANTimings(HAL_RCC_GetPCLK1Freq(), target_bitrate, &timings);
+  Serial.print("ComputeCANTimings result=");
+  Serial.println(result);
+  if (result) while(true);  
+  CAN1->BTR = (((timings.resynchronization_jump_width - 1U) &    3U) << 24U) |
+              (((timings.time_segment_1 - 1U)               &   15U) << 16U) |
+              (((timings.time_segment_2 - 1U)               &    7U) << 20U) |
+              ((timings.baud_rate_prescaler - 1U)           & 1023U);
 
   // Configure Filters to default values
   CAN1->FMR  |=   0x1UL;                // Set to filter initialization mode
   CAN1->FMR  &= 0xFFFFC0FF;             // Clear CAN2 start bank
 
   // bxCAN has 28 filters.
-  // These filters are used for both CAN1 and CAN2.
+  // These filters are shared by both CAN1 and CAN2.
   // STM32F103 has only CAN1, so all 28 are used for CAN1
   CAN1->FMR  |= 0x1C << 8;              // Assign all filters to CAN1
 
@@ -345,8 +509,10 @@ const long interval = 1000;           // transmission interval (milliseconds)
 
 void setup() {
   Serial.begin(115200);
+ 
   //bool ret = CANInit(CAN_500KBPS, 0);  // CAN_RX mapped to PA11, CAN_TX mapped to PA12
   bool ret = CANInit(CAN_500KBPS, 2);  // CAN_RX mapped to PB8, CAN_TX mapped to PB9
+  //bool ret = CANInit(CAN_500KBPS, 3);  // CAN_RX mapped to PD0, CAN_TX mapped to PD1
   //bool ret = CANInit(CAN_1000KBPS, 0);  // CAN_RX mapped to PA11, CAN_TX mapped to PA12
   //bool ret = CANInit(CAN_1000KBPS, 2);  // CAN_RX mapped to PB8, CAN_TX mapped to PB9
   //bool ret = CANInit(CAN_1000KBPS, 3);  // CAN_RX mapped to PD0, CAN_TX mapped to PD1
